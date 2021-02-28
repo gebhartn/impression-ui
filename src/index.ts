2bf4bf20bb9255da83e4af0e97cc929e8e0366d2
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-02-28 15:46:53 -0800

    Clean up server instance

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import express from 'express';
 
-let app = require('./server').default;
+let { server } = require('./server');
 
 if (module.hot) {
     module.hot.accept('./server', () => {
-        console.log('🔁  HMR Reloading `./server`...');
+        console.log('R  HMR Reloading `./server`...');
         try {
-            app = require('./server').default;
+            server = require('./server');
         } catch (error) {
             console.error(error);
         }
     });
-    console.info('✅  Server-side HMR Enabled!');
+    console.info('OK  Server-side HMR Enabled!');
 }
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 export default express()
-    .use((req, res) => app.handle(req, res))
+    .use((req, res) => server.handle(req, res))
     .listen(port, () => {
         console.log(`> App started http://localhost:${port}`);
     });
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,53 +1,15 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
 
-import { App } from '../client/pages';
+import { api } from './api';
+import { app } from './app';
+import { middleware } from './middleware';
 
-let assets: any;
+export const server = express();
 
-const syncLoadAssets = () => {
-    assets = require(process.env.RAZZLE_ASSETS_MANIFEST!);
-};
-syncLoadAssets();
+server.disable('x-powered-by');
 
-const server = express()
-    .disable('x-powered-by')
-    .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
-    .get('/*', (req: express.Request, res: express.Response) => {
-        const context = {};
-        const markup = renderToString(
-            <StaticRouter context={context} location={req.url}>
-                <App />
-            </StaticRouter>,
-        );
-        res.send(
-            `
-<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charSet='utf-8' />
-        <title>Razzle TypeScript</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-            assets.client.css
-                ? `<link rel="stylesheet" href="${assets.client.css}">`
-                : ''
-        }
-          ${
-              process.env.NODE_ENV === 'production'
-                  ? `<script src="${assets.client.js}" defer></script>`
-                  : `<script src="${assets.client.js}" defer crossorigin></script>`
-          }
-    </head>
-    <body>
-        <div id="impression-ui">${markup}</div>
-    </body>
-</html>
-`,
-        );
-    });
+middleware.forEach(middleware => server.use(middleware));
 
-export default server;
+server.use('/public', express.static(process.env.RAZZLE_PUBLIC_DIR!));
+server.use('/api', api);
+server.use('*', app);
